684a38538220275fd1004f4513c8eaa6a37f205e
Bruno Chiconato <brunochiconato01@gmail.com>
2024-12-01 15:59:10 -0300

    Segunda versão do exercício 'tabuada.py' realizada.

--- tabuada.py
+++ tabuada.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+
+    1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
 ...
----------------
-Tabuada do 2
-2
-4
-6
+##################
+---Tabuada do 2---
+
+    2 x 1 = 1
+    2 x 2 = 4
+    2 x 3 = 6
 ...
 """
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Bruno Chiconato"
 
 numbers = list(range(1, 11))
 
 for number in numbers:
-    print("Tabuada do:", number)
+    header = f"Tabuada do {number}"
+    len_header = len(header) + 6
+    print(f"{header:-^{len_header}}\n")
+
     for multiplier in numbers:
-        print(multiplier * number)
-    print("------------")
+        result = number * multiplier
+        msg = f"{number} x {multiplier} = {result}"
+        print(f"{msg:^{len_header}}")
+
+    print("\n", "#" * len_header)

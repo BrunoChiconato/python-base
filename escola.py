f5e49bd93aa602dd1d351b7cf9af8d1d744a5a92
Bruno Chiconato <brunochiconato01@gmail.com>
2024-12-07 19:21:41 -0300

    Versão 0.1.1 do 'escola.py' utilizando dicionários e sets realizado.

--- escola.py
+++ escola.py
@@ -5,7 +5,7 @@ Imprimir a lista de crianças agrupadas por sala
 que frequenta cada uma das atividades.
 """
 __author__ = "Bruno Chiconato"
-__version__ = "0.0.1"
+__version__ = "0.1.1"
 
 sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
 sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
@@ -14,64 +14,20 @@ aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
 aula_musica = ["Erik", "Carlos", "Maria"]
 aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
 
-aulas = ["inglês", "música", "dança"]
+dict_classes = {
+    "Ingles": aula_ingles,
+    "Musica": aula_musica,
+    "Danca": aula_danca
+}
 
-aluno_sala1_ingles = []
-aluno_sala1_musica = []
-aluno_sala1_danca = []
+for classes, students in dict_classes.items():
+    print(f"Alunos da atividade de {classes}\n")
+    print("-" * 40)
 
-aluno_sala2_ingles = []
-aluno_sala2_musica = []
-aluno_sala2_danca = []
+    students_classroom_1 = set(sala1) & set(students)
+    students_classroom_2 = set(sala2) & set(students)
 
-for aluno in sala1:
-    if aluno in aula_ingles:
-        aluno_sala1_ingles.append(aluno)
-    if aluno in aula_musica:
-        aluno_sala1_musica.append(aluno)
-    if aluno in aula_danca:
-        aluno_sala1_danca.append(aluno)
+    print(f"Sala 1 {students_classroom_1}")
+    print(f"Sala 2 {students_classroom_2}\n")
 
-for aluno in sala2:
-    if aluno in aula_ingles:
-        aluno_sala2_ingles.append(aluno)
-    if aluno in aula_musica:
-        aluno_sala2_musica.append(aluno)
-    if aluno in aula_danca:
-        aluno_sala2_danca.append(aluno)
-
-for aula in aulas:
-    if aula == "inglês":
-        print(f"Alunos da aula de {aula}:")
-        print("Sala 1")
-
-        for aluno in aluno_sala1_ingles:
-            print(f"- {aluno}")
-
-        print("Sala 2")
-        for aluno in aluno_sala2_ingles:
-            print(f"- {aluno}")
-        
-        print()
-    elif aula == "música":
-        print(f"Alunos da aula de {aula}:")
-        print("Sala 1")
-
-        for aluno in aluno_sala1_musica:
-            print(f"- {aluno}")
-
-        print("Sala 2")
-        for aluno in aluno_sala2_musica:
-            print(f"- {aluno}")
-        
-        print()
-    elif aula == "dança":
-        print(f"Alunos da aula de {aula}:")
-        print("Sala 1")
-
-        for aluno in aluno_sala1_danca:
-            print(f"- {aluno}")
-
-        print("Sala 2")
-        for aluno in aluno_sala2_danca:
-            print(f"- {aluno}")
+    print("#" * 40)

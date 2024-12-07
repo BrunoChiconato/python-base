48c46bbea56f8b8ec81e40921953bdc028eb7cf6
Bruno Chiconato <brunochiconato01@gmail.com>
2024-12-07 17:55:57 -0300

    Versão 1.2 do 'hello.py' multi linguas realizado.

--- hello.py
+++ hello.py
@@ -16,18 +16,19 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Bruno Chiconato"
 __license__ = "Unlicense"
 
 import os
 
 current_language = os.getenv("LANG","en_US")[:5]
-msg = "Hello, World!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "es_SP": "Hola, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "fr_FR": "Bonjour, Monde!"
+}
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-
-print(msg)
+print(msg[current_language])

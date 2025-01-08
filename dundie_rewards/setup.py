691ce1c6ddcb1867084ad368b6bf1480727ef0bf
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-07 21:52:18 -0300

    Updates
    Added a setup.py file.
    
    Added the main package 'dundie' for the project.
    
    Added a 'docs' folder for the documentation of the project.

---
+++ dundie_rewards/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="dundie",
+    version="0.1.0", # semantic version
+    description="Reward Point System for Dunder Mifflin",
+    author="Bruno Chiconato",
+    packages=["dundie"],
+)

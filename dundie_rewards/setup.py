08629d04b8064c301bbb2496855b449c47cf062d
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-12 12:56:33 -0300

    Updates
    Definied the load function to read a file and print its contents
    
    Defined the main function to parse arguments and call the subcommand function.

--- dundie_rewards/setup.py
+++ dundie_rewards/setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup
 
-
 setup(
     name="dundie",
-    version="0.1.0", # semantic version
+    version="0.1.0",  # semantic version
     description="Reward Point System for Dunder Mifflin",
     author="Bruno Chiconato",
     packages=["dundie"],
-    entry_points={
-        "console_scripts": [
-            "dundie = dundie.__main__:main"
-        ]
-    }
+    entry_points={"console_scripts": ["dundie = dundie.__main__:main"]},
 )

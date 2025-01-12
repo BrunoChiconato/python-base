4768c115c7d1141923518abeea581756799c8887
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-12 12:34:16 -0300

    Turned to installable binary.

--- dundie_rewards/setup.py
+++ dundie_rewards/setup.py
@@ -7,4 +7,9 @@ setup(
     description="Reward Point System for Dunder Mifflin",
     author="Bruno Chiconato",
     packages=["dundie"],
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )

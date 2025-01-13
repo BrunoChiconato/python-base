62862e4cc8232f191f2d72c6d40129c7bfba4b3c
Bruno Chiconato <brunochiconato01@gmail.com>
2025-01-13 20:58:56 -0300

    Updates
    Splited the __main__ module into two separate modules: dundie/cli.py and dundie/core.py.
    
    Added log module to dundie/utils/log.py.
    
    Added tests for load function in dundie_rewards/tests/test_load.py.

---
+++ dundie_rewards/dundie/cli.py
@@ -0,0 +1,27 @@
+import argparse
+
+from dundie.core import load
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Dundie Mifflin Rewards CLI",
+        epilog="Enjoy and use with caution!",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="Subcommand to run",
+        choices=("load", "show", "send"),
+        default="help",
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="Filepath to load",
+        default=None,
+    )
+
+    args = parser.parse_args()
+
+    print(*globals()[args.subcommand](args.filepath))
